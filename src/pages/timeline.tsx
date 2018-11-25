78607aa437b50020985bce9d7f649a6d76ae1041
juliohds <tecjuliohenrique@gmail.com>
2018-11-25 07:46:06 -0200

    img

---
+++ src/pages/timeline.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+import { Link } from "react-router-dom";
+
+import { Row, Col, Container } from "reactstrap";
+
+import mapa from "./../images/map.png";
+
+export default class TimeLine extends React.Component {
+  render() {
+    return (
+      <Container fluid={true} className="institutional-background">
+        <Row>
+          <Col xs="12">
+            <div>
+              <div>
+                <h2>julio</h2>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
