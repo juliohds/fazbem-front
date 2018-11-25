474756ea3b3b8632dc1ca34e890e43cb421f6e76
juliohds <tecjuliohenrique@gmail.com>
2018-11-25 04:33:57 -0200

    mapa

--- src/pages/mapacompra.tsx
+++ src/pages/mapacompra.tsx
@@ -6,9 +6,20 @@ import { IApplicationState, IConnectedReduxProps } from "../store";
 import { RouteComponentProps } from "react-router-dom";
 import { connect } from "react-redux";
 
-import mapa from "../../images/map.png";
+import {
+  Card,
+  Row,
+  Col,
+  Button,
+  CardImg,
+  CardText,
+  CardBody,
+  CardLink,
+  CardTitle,
+  CardSubtitle
+} from "reactstrap";
 
-import { Button, Col, Row } from "reactstrap";
+import mapa from "./../images/map.png";
 
 type AllProps = RouteComponentProps<{}> & IConnectedReduxProps;
 
@@ -16,11 +27,30 @@ class MapaCompra extends React.Component<AllProps> {
   public render() {
     return (
       <div className="wrapper">
-        <div className="container">
+        <div className="mt-100">
           <Row>
-            <Col md={{ size: 8, offset: 2 }}>
-              <img src={mapa} alt="Mapa" />
-              <h1>oi</h1>
+            <Col md={{ size: 6, offset: 3 }}>
+              <div>
+                <Card>
+                  <CardBody>
+                    <CardTitle>Pontos de Vendas</CardTitle>
+                    <CardSubtitle>Proximos a Você</CardSubtitle>
+                  </CardBody>
+                  <img width="100%" src={mapa} alt="Card image cap" />
+                  <CardBody>
+                    <CardText>
+                      Os Alimentos ficam sujeitos a serem vendidos antes que
+                      você chegue no local.
+                    </CardText>
+                    <Link to="/">
+                      <Button color="success">Comprar</Button>
+                    </Link>
+                    <Link to="/">
+                      <Button color="danger">Voltar</Button>
+                    </Link>
+                  </CardBody>
+                </Card>
+              </div>
             </Col>
           </Row>
         </div>
